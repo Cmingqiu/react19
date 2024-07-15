2757295de9ad62fe303a22a22226c1cfcedb39f3
chenmq <1075124372@qq.com>
2024-07-15 21:16:21 +0800

    feat: 修改404页面

--- src/views/NOT_FOUND/index.jsx
+++ src/views/NOT_FOUND/index.jsx
@@ -1,4 +1,8 @@
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { Button } from 'antd';
+
+import notFoundImg from '@/images/404.svg';
 
 export default function NOT_FOUND() {
   const navigate = useNavigate();
@@ -6,11 +10,23 @@ export default function NOT_FOUND() {
     navigate('/');
   }
   return (
-    <>
-      404
-      <div>
-        <button onClick={goHome}>回到首页</button>
-      </div>
-    </>
+    <NOT_FOUND_WRAP>
+      <img src={notFoundImg} alt='' srcSet='' />
+      <Button type='link' onClick={goHome}>
+        网页不翼而飞 先回首页吧~
+      </Button>
+    </NOT_FOUND_WRAP>
   );
 }
+
+const NOT_FOUND_WRAP = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 400px;
+    height: 300px;
+  }
+`;
