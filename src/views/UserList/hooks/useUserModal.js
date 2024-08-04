04bb1b80253f887da17595d30709881d462ff93d
chenmq <1075124372@qq.com>
2024-08-04 16:43:58 +0800

    feat: 提交表单前校验

--- src/views/UserList/hooks/useUserModal.js
+++ src/views/UserList/hooks/useUserModal.js
@@ -26,31 +26,35 @@ export default (currentRow, getUsers) => {
     };
   }
 
-  async function ok() {
-    const formData = modalRef.current.form.getFieldsValue();
-    if (modal.modalType === 2) {
-      formData.id = currentRow.current._id;
-    }
-    const methods = {
-      1: apiAddUser,
-      2: apiUpdateUser
-    };
-    const res = await methods['' + modal.modalType](formData);
-    console.log('ok： ', res);
-    message.success(`${modal.modalType === 1 ? '添加' : '修改'}成功`);
-    onCancel();
-    getUsers();
+  function ok() {
+    const form = modalRef.current.form;
+    form.validateFields().then(async () => {
+      const formData = form.getFieldsValue();
+      if (modal.modalType === 2) {
+        formData.id = currentRow.current._id;
+      }
+      const methods = {
+        1: apiAddUser,
+        2: apiUpdateUser
+      };
+      const res = await methods['' + modal.modalType](formData);
+      console.log('ok： ', res);
+      message.success(`${modal.modalType === 1 ? '添加' : '修改'}成功`);
+      onCancel();
+      getUsers();
+    });
   }
   function onCancel() {
     setModal({ ...modal, modalShow: false });
   }
 
   function afterOpenChange(visible) {
+    const form = modalRef.current.form;
     if (visible && modal.modalType === 2) {
       const { name, password, age, roles } = currentRow.current;
-      modalRef.current.form.setFieldsValue({ name, password, age, roles });
+      form.setFieldsValue({ name, password, age, roles });
     } else {
-      modalRef.current.form.resetFields();
+      form.resetFields();
     }
   }
 
