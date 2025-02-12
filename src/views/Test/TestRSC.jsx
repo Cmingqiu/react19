1f4adf5d2779d9308b5f273ad98450bb9b18770c
chenmingqiu <1075@qq.com>
2025-02-12 13:55:15 +0800

    测试执行顺序

--- src/views/Test/TestRSC.jsx
+++ src/views/Test/TestRSC.jsx
@@ -1,9 +1,34 @@
-'use server';
+// ////////////////////
+// 'use server';
+
+import { useEffect, useState } from 'react';
 
 export default function TestRSC() {
+  const [count, setCount] = useState(0);
+  console.log('render', count, new Date().toLocaleString());
+  useEffect(() => {
+    console.log('mount', new Date().toLocaleString());
+    setTimeout(() => {
+      // setCount(10);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    // setCount(count => count + 1);
+    console.log('count change', new Date().toLocaleString());
+  }, [count]);
+
+  function change() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
       <h2>TestSrc</h2>
+      <div>{count}</div>
+      <button type='button' onClick={change}>
+        change
+      </button>
     </div>
   );
 }
